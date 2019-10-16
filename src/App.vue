aaa328a641323633f882f4b12b699fead6231b87
poushitaguha <pguha.kolkata@gmail.com>
2019-10-16 16:05:51 -0400

    Resolve conflicts

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,42 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-<<<<<<< Updated upstream
     <Poushita msg="Welcome to Poushita's Vue Component"/>
-=======
     <PerrysComponent msg="Perry's clock"/>
->>>>>>> Stashed changes
   </div>
 </template>
 
 <script>
-<<<<<<< Updated upstream
 import Poushita from "./components/Poushita.vue";
-=======
-import PerrysComponent from './components/PerrysComponent.vue';
->>>>>>> Stashed changes
+import PerrysComponent from "./components/PerrysComponent.vue";
 
 export default {
   name: "app",
   components: {
-<<<<<<< Updated upstream
-    Poushita
-=======
+    Poushita,
     PerrysComponent
->>>>>>> Stashed changes
   }
 };
 </script>
 
 <style>
-<<<<<<< Updated upstream
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
-=======
+}
 html {
-    background: #018DED url(http://unsplash.it/1500/1000?image=881&blur=50);
-    background-size: cover;
-    font-family: 'Helvetica Neue';
-    text-align: center;
-    font-size: 10px;
+  background: #018ded url(http://unsplash.it/1500/1000?image=881&blur=50);
+  background-size: cover;
+  font-family: "Helvetica Neue";
+  text-align: center;
+  font-size: 10px;
 }
 
 body {
-    font-size: 2rem;
-    display: flex;
-    flex: 1;
-    min-height: 100vh;
-    align-items: center;
-    justify-content: center;
+  font-size: 2rem;
+  display: flex;
+  flex: 1;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: center;
 }
-
-/* #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
->>>>>>> Stashed changes
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
 </style>
